9693ef1bf286584e017336dca5fd1ce34aa39081
pointpeng <pointpeng@tencent.com>
2021-11-26 10:53:20 +0800

    feat: add toca scene

--- temp/declarations/jsb.d.ts
+++ temp/declarations/jsb.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="D:\CocosCreator-v3.3.2-win32-100820\resources\resources\3d\engine\@types\jsb.d.ts"/>
+/// <reference path="/Applications/CocosCreator/Creator/3.3.2/CocosCreator.app/Contents/Resources/resources/3d/engine/@types/jsb.d.ts"/>
