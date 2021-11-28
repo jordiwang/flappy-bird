33d82f7b473dda3f5e91631e21e90c936f55249c
jordiwang <jordiwang@outlook.com>
2021-11-28 14:32:09 +0800

    flappy bird游戏逻辑实现

--- assets/Script/BirdController.ts
+++ assets/Script/BirdController.ts
@@ -5,57 +5,64 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
+import { MainController, GAME_STATE } from './MainController';
+import { AudioController, SOUND_TYPE } from './AudioController';
+
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class BirdController extends cc.Component {
+    @property(MainController)
+    mainController: MainController = null;
 
+    @property(AudioController)
+    audioController: AudioController = null;
 
     //Speed of bird
     speed: number = 0;
 
-
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
+    rigidBody: cc.RigidBody = null;
 
     onLoad() {
         cc.Canvas.instance.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
     }
 
-    start() {
-
-    }
+    start() {}
 
     update(dt: number) {
-        this.speed -= 0.05
+        if (this.mainController.gameState === GAME_STATE.GAME_PLAYING) {
+            this.speed -= 0.05;
 
-        let position = this.node.getPosition()
+            let position = this.node.getPosition();
 
-        position.y += this.speed
+            position.y += this.speed;
 
-        let angle = (this.speed / 2) * 30
+            let angle = (this.speed / 2) * 30;
 
-        if (angle >= 30) {
-            angle = 30
+            if (angle >= 30) {
+                angle = 30;
+            }
+
+            this.node.angle = angle;
+            this.node.setPosition(position);
         }
-
-        this.node.angle = (angle);
-        this.node.setPosition(position);
-
     }
 
-
     onTouchStart(event: cc.Event.EventTouch) {
-
-        console.log(';;;;;;;;;;;;;;;;');
-
-
         this.speed = 2;
+
+        this.audioController.playSound(SOUND_TYPE.E_Sound_Fly);
+    }
+
+    onCollisionEnter(other: cc.Collider, self: cc.Collider) {
+        if (other.tag === 0) {
+            this.speed = 0;
+
+            this.mainController.gameOver();
+        }
+        // collider tag is 1, that means the bird cross a pipe, then add score
+        else if (other.tag === 1) {
+            this.mainController.addScore();
+        }
     }
 }
--- assets/Script/MainController.ts
+++ assets/Script/MainController.ts
@@ -5,40 +5,134 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
+import { AudioController, SOUND_TYPE } from './AudioController';
 
 const { ccclass, property } = cc._decorator;
 
+const MIN_Y = -120;
+const MAX_Y = 120;
+
+export enum GAME_STATE {
+    GAME_INIT,
+    GAME_PLAYING,
+    GAME_END,
+}
+
 @ccclass
-export default class NewClass extends cc.Component {
-
-
+export class MainController extends cc.Component {
     @property(cc.Sprite)
-    bgList: cc.Sprite[] = [null, null]
+    bgList: cc.Sprite[] = [null, null];
+
+    @property(cc.Node)
+    pipeContainer: cc.Node = null;
 
     @property(cc.Prefab)
     pipePrefab: cc.Prefab = null;
 
-    // LIFE-CYCLE CALLBACKS:
+    @property(cc.Sprite)
+    gameOverSprite: cc.Sprite = null;
 
-    // onLoad () {}
+    @property(cc.Sprite)
+    gameTitleSprite: cc.Sprite = null;
+
+    @property(cc.Button)
+    buttonStart: cc.Button = null;
+
+    @property(AudioController)
+    audioController: AudioController = null;
+
+    @property(cc.Label)
+    labelScore: cc.Label = null;
+
+    pipeList: cc.Node[] = [null, null, null];
+
+    gameState: GAME_STATE = GAME_STATE.GAME_INIT;
+
+    gameScore: number;
+
+    onLoad() {
+        this.init();
+    }
 
     start() {
+        for (let i = 0; i < this.pipeList.length; i++) {
+            const pipe = cc.instantiate(this.pipePrefab);
 
+            this.pipeList[i] = pipe;
+
+            pipe.setPosition(170 + 200 * i, MIN_Y + Math.random() * (MAX_Y - MIN_Y));
+
+            this.pipeContainer.addChild(pipe);
+        }
     }
 
     update(dt: number) {
-        // move the background node
-        for (let i = 0; i < this.bgList.length; i++) {
-            let bgPositoin = this.bgList[i].node.getPosition()
+        if (this.gameState === GAME_STATE.GAME_PLAYING) {
+            // move the background node
+            for (let i = 0; i < this.bgList.length; i++) {
+                let bgPositoin = this.bgList[i].node.getPosition();
 
-            bgPositoin.x -= 1.0
+                bgPositoin.x -= 1.0;
 
-            if (bgPositoin.x <= -288) {
-                bgPositoin.x = 288
+                if (bgPositoin.x <= -288) {
+                    bgPositoin.x = 288;
+                }
+
+                this.bgList[i].node.setPosition(bgPositoin);
             }
 
-            this.bgList[i].node.setPosition(bgPositoin);
+            // move pipes
+            for (let i = 0; i < this.pipeList.length; i++) {
+                let position = this.pipeList[i].getPosition();
 
+                position.x -= 1.0;
+
+                if (position.x <= -170) {
+                    position.x = 430;
+
+                    position.y = MIN_Y + Math.random() * (MAX_Y - MIN_Y);
+                }
+
+                this.pipeList[i].setPosition(position);
+            }
         }
+    }
+
+    init() {
+        cc.director.getCollisionManager().enabled = true;
+
+        this.gameState = GAME_STATE.GAME_INIT;
+
+        this.gameTitleSprite.node.active = true;
+
+        this.gameOverSprite.node.active = false;
+
+        this.buttonStart.node.active = true;
+
+        this.gameScore = 0;
+        this.labelScore.string = this.gameScore.toString();
+
+        this.buttonStart.node.on(cc.Node.EventType.TOUCH_END, this.gameStart, this);
+    }
+
+    gameStart() {
+        this.gameState = GAME_STATE.GAME_PLAYING;
+
+        this.gameTitleSprite.node.active = false;
+
+        this.buttonStart.node.active = false;
+    }
+
+    gameOver() {
+        this.gameState = GAME_STATE.GAME_END;
+
+        this.audioController.playSound(SOUND_TYPE.E_Sound_Die);
+
+        this.gameOverSprite.node.active = true;
+    }
+
+    addScore() {
+        this.gameScore += 1;
+        this.labelScore.string = this.gameScore.toString();
     }
 }
